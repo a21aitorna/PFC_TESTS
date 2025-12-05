f93ac8aa32b9a8f87f1f7e525e68c426309dbdcf
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-12-05 18:36:32 +0100

    Adaptacion driver

--- frontend-testing/src/test/java/manager/DriverManager.java
+++ frontend-testing/src/test/java/manager/DriverManager.java
@@ -3,8 +3,11 @@ package manager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverManager {
@@ -22,35 +25,53 @@ public class DriverManager {
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                    driver = new ChromeDriver(chromeOptions);
                     break;
+
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--window-size=1920,1080");
+                    driver = new FirefoxDriver(firefoxOptions);
                     break;
+
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--no-sandbox");
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                    driver = new EdgeDriver(edgeOptions);
                     break;
+
                 case "safari":
                     WebDriverManager.safaridriver().setup();
-                    driver = new SafariDriver();
+                    driver = new SafariDriver(); // Safari headless no disponible en Linux
                     break;
+
                 default:
-                    throw new RuntimeException("Navegador no soportado: "+ browser);
+                    throw new RuntimeException("Navegador no soportado: " + browser);
             }
-            driver.manage().window().maximize();
-           // driver = SlowWebDriver.slow(driver, 600);
+
+            // driver.manage().window().maximize(); // Ya definimos tamaño en headless
         }
-        return  driver;
+        return driver;
     }
 
     /**
-     * Cerrar el navegdor y eliminar instancia del driver
+     * Cerrar el navegador y eliminar instancia del driver
      */
-    public static  void quitDriver() {
-        if(driver != null) {
-            driver.quit();;
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
             driver = null;
         }
     }
